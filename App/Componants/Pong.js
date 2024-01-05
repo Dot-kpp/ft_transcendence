90f2137fbaa231a6ae4b44586e463d9222c0a6cb
Jeremie Pilotte <Jeremie.p23@gmail.com>
2024-01-05 16:52:23 -0500

    simple 2 player pong added

--- App/Componants/Pong.js
+++ App/Componants/Pong.js
@@ -1,109 +1,146 @@
 // Get the canvas element from the HTML
 const canvas = document.getElementById('pongCanvas');
 const context = canvas.getContext('2d');
-let rightArrow = false;
-let leftArrow = false;
 
-// Event listeners for keydown and keyup events
-document.addEventListener('keydown', function(event) {
-    switch(event.keyCode) {
-        case 39: // Right arrow key
-            rightArrow = true;
-            break;
-        case 37: // Left arrow key
-            leftArrow = true;
-            break;
-    }
-}, false);
-
-document.addEventListener('keyup', function(event) {
-    switch(event.keyCode) {
-        case 39: // Right arrow key
-            rightArrow = false;
-            break;
-        case 37: // Left arrow key
-            leftArrow = false;
-            break;
-    }
-}, false);
-
-// Create the pong paddle
-const paddleWidth = 75, paddleHeight = 15;
-const paddle = {
-    x: canvas.width / 2 - paddleWidth / 2,
-    y: canvas.height - paddleHeight,
+// Create the pong paddles
+const paddleWidth = 15, paddleHeight = 75;
+const paddle1 = {
+    x: 0,
+    y: canvas.height / 2 - paddleHeight / 2,
     width: paddleWidth,
     height: paddleHeight,
-    dx: 2
+    dy: 2
 };
+
+const paddle2 = {
+    x: canvas.width - paddleWidth,
+    y: canvas.height / 2 - paddleHeight / 2,
+    width: paddleWidth,
+    height: paddleHeight,
+    dy: 2
+};
+
+// Variables to hold arrow key states
+let upArrow = false;
+let downArrow = false;
+let wKey = false;
+let sKey = false;
 
 // Create the pong ball
 const ball = {
     x: canvas.width / 2,
-    y: paddle.y - 10,
+    y: canvas.height / 2,
     radius: 10,
-    speed: 2,
     dx: 2,
     dy: -2
 };
 
 // Draw the paddle
 function drawPaddle(x, y, width, height) {
-    context.fillStyle = '#000';
-    context.fillRect(x, y, width, height);
-    context.strokeStyle = '#000';
-    context.strokeRect(x, y, width, height);
+    context.beginPath();
+    context.rect(x, y, width, height);
+    context.fillStyle = '#0095DD';
+    context.fill();
+    context.closePath();
 }
 
 // Draw the ball
 function drawBall(x, y, radius) {
     context.beginPath();
-    context.arc(x, y, radius, 0, Math.PI * 2, false);
-    context.fillStyle = '#000';
+    context.arc(x, y, radius, 0, Math.PI*2);
+    context.fillStyle = '#0095DD';
     context.fill();
-    context.strokeStyle = '#000';
-    context.stroke();
     context.closePath();
 }
 
-// Move the paddle
-function movePaddle() {
-    if(rightArrow && paddle.x + paddle.width < canvas.width) {
-        paddle.x += paddle.dx;
-    } else if(leftArrow && paddle.x > 0) {
-        paddle.x -= paddle.dx;
+// Event listeners for keydown and keyup events
+document.addEventListener('keydown', function(event) {
+    switch(event.keyCode) {
+        case 38: // Up arrow key
+            upArrow = true;
+            break;
+        case 40: // Down arrow key
+            downArrow = true;
+            break;
+        case 87: // 'W' key
+            wKey = true;
+            break;
+        case 83: // 'S' key
+            sKey = true;
+            break;
+    }
+}, false);
+
+document.addEventListener('keyup', function(event) {
+    switch(event.keyCode) {
+        case 38: // Up arrow key
+            upArrow = false;
+            break;
+        case 40: // Down arrow key
+            downArrow = false;
+            break;
+        case 87: // 'W' key
+            wKey = false;
+            break;
+        case 83: // 'S' key
+            sKey = false;
+            break;
+    }
+}, false);
+
+// Move the paddles
+function movePaddles() {
+    if(upArrow && paddle1.y > 0) {
+        paddle1.y -= paddle1.dy;
+    } else if(downArrow && paddle1.y + paddle1.height < canvas.height) {
+        paddle1.y += paddle1.dy;
+    }
+
+    if(wKey && paddle2.y > 0) {
+        paddle2.y -= paddle2.dy;
+    } else if(sKey && paddle2.y + paddle2.height < canvas.height) {
+        paddle2.y += paddle2.dy;
     }
 }
-
+function resetBall() {
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.dx = 2;
+    ball.dy = -2;
+}
 // Move the ball
 function moveBall() {
     ball.x += ball.dx;
     ball.y += ball.dy;
 
-    if(ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
-        ball.dx *= -1; // Ball change direction
-    }
-
-    if(ball.y + ball.radius > canvas.height) {
-        alert('Game Over');
-        document.location.reload();
-        clearInterval(interval); // Needed for Chrome to end game
-    } else if(ball.y - ball.radius < 0) {
+    if(ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
         ball.dy *= -1; // Ball change direction
     }
 
     // Ball and paddle collision detection
-    if(ball.y + ball.radius > paddle.y && ball.y - ball.radius < paddle.y + paddle.height && ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
-        ball.dy *= -1;
+    if(ball.dx < 0 && ball.x - ball.radius < paddle1.x + paddle1.width && ball.y > paddle1.y && ball.y < paddle1.y + paddle1.height) {
+        ball.dx *= -1;
+    } else if(ball.dx > 0 && ball.x + ball.radius > paddle2.x && ball.y > paddle2.y && ball.y < paddle2.y + paddle2.height) {
+        ball.dx *= -1;
+    }
+
+    // Check if the ball is out of bounds
+    if(ball.x + ball.radius < 0) {
+        alert('Player 2 wins!');
+        resetBall();
+    } else if(ball.x - ball.radius > canvas.width) {
+        alert('Player 1 wins!');
+        resetBall();
     }
 }
 
 // Update the canvas
 function update() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    drawPaddle(paddle.x, paddle.y, paddle.width, paddle.height);
+    drawPaddle(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+    drawPaddle(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
     drawBall(ball.x, ball.y, ball.radius);
-    movePaddle();
+    movePaddles();
     moveBall();
 }
 
